e508c39759f5efe81f3f5c5ddc961c5cba356333
Aqilah <qilasolo20@gmail.com>
2025-08-29 11:11:42 +0700

    Add files via upload

---
+++ aqilah littaniya.py
@@ -0,0 +1,13 @@
+
+# segitiga kebalik menggunakan while
+
+i = 9
+while i >= 1:
+    print('*' * i)
+    i -= 2
+
+# segitiga menggunakan while
+i = 1
+while i <= 10:
+    print('*' * i)
+    i += 2
